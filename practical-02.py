7faed5c272811f5fd90868272b0ee2879e0b7d5f
Abhishek Gautam <Gautam.abhishek7100@gmail.com>
2022-08-26 01:23:37 +0530

    Update

--- practical-02.py
+++ practical-02.py
@@ -8,7 +8,7 @@ print("Subtraction of a and b : ", a-b)
 print("Multiplication of a and b : ", a*b)
 print("Division of a and b : ", a/b)
 print("Remainder of a and b : ", a % b)
-print("Exponent of a and b : ", a**b)
+print("Exponent of a and b : ", a ** b)
 
 # or
 
