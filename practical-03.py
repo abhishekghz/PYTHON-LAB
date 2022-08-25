a7db20baba8a7d3899a9be4b8a2c585ea9c41ff2
Abhishek Gautam <Gautam.abhishek7100@gmail.com>
2022-08-26 01:28:28 +0530

    Updated commit

--- practical-03.py
+++ practical-03.py
@@ -1,9 +1,9 @@
 # PRACTICAL 03
 # Write a program to create, concatenate and print a string and accessing sub-string from a given string.
 
-s1 = input("Enter first String : ")
-s2 = input("Enter second String : ")
-print("First string is : ", s1)
-print("Second string is : ", s2)
-print("concatenations of two strings :", s1+s2)
-print("Substring of given string :", s1[1:6])
+a = input("Enter first String : ")
+b = input("Enter second String : ")
+print("First string is : ", a)
+print("Second string is : ", b)
+print("concatenations of two strings :", a+b)
+print("Substring of given string :", a[1:6])
