d74bcdd431bcb4e2d146189b054b51d8ff3c60f5
abhishekgautam <gautam.abhishek7100@gmail.com>
2022-09-07 18:09:17 +0530

    Updated commit

---
+++ practical-04.py
@@ -0,0 +1,6 @@
+# PRACTICAL 04
+# Write a python script to print the current date in the following format “Sun May 29 02:26:23 IST 2017” ?
+
+import time
+curr_time = time.localtime()
+print(time.strftime("%a %b %d %H:%M:%S %Z %Y", curr_time))
