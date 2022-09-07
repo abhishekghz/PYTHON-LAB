04bdb6a20cdebee600fa490efa30d5275fa5361d
abhishekgautam <gautam.abhishek7100@gmail.com>
2022-09-07 18:08:12 +0530

    Updated commit

--- practical-02.py
+++ practical-02.py
@@ -10,11 +10,4 @@ print("Division of a and b : ", a/b)
 print("Remainder of a and b : ", a % b)
 print("Exponent of a and b : ", a ** b)
 
-# Write a program to create, concatenate and print a string and accessing sub-string from a given string.
 
-a = input("Enter first String : ")
-b = input("Enter second String : ")
-print("First string is : ", a)
-print("Second string is : ", b)
-print("concatenations of two strings :", a+b)
-print("Substring of given string :", a[1:6])
